82c9b82149914543c4b961ce7b36d2355bd6a71f
philmcevatt <philmcevatt@yahoo.com>
2026-07-25 23:19:45 +0100

    Update philfedgemini.sh

--- philfedgemini.sh
+++ philfedgemini.sh
@@ -14,7 +14,7 @@ exec 2>&1
 # TOGGLES
 ############################################################
 
-INSTALL_NVIDIA=false
+INSTALL_NVIDIA=true
 INSTALL_VIRT=true
 INSTALL_MAXWELL_FIX=true
 INSTALL_OPENRAZER=true
